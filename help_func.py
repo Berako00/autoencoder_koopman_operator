8bc15f04e5b3b4dfe09604fc9c05094a73fba5bf
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-04-11 08:50:23 +0200

    Update help_func.py

--- help_func.py
+++ help_func.py
@@ -5,21 +5,35 @@ import os
 from loss_func import custom_loss
 
 def get_model_path(i):
-    path1 = f"/home/trarity/master/koopman_operator/data/Autoencoder_model_params{i}.pth"
-    path2 = f"C:/Users/jokin/Desktop/Uni/Aalborg/Master/Masters_Thesis/Path/Autoencoder_model_params{i}.pth"
-    path3 = f"/content/drive/My Drive/Colab Notebooks/Autoencoder_model_params{i}.pth"
-    path4 = f"/content/drive/MyDrive/Colab Notebooks/Autoencoder_model_params{i}.pth"
-    path5 = f"/home/trarity/koopman_operator/data/Autoencoder_model_params{i}.pth"
+    windows_dir1 = r"C:\Users\jokin\Desktop\Uni\Aalborg\Master\Masters_Thesis"
+    windows_dir2 = r"C:\Users\Labuser\Desktop\WAPNN"
+    linux_dir   = "/home/trarity/master/koopman_control/data"
+    linux_dir2   = "/home/trarity/state_koopman_operator"
+    colab_dir1  = "/content/drive/My Drive/Colab Notebooks"
+    colab_dir2  = "/content/drive/MyDrive/Colab Notebooks"  
+    ucloud = "/work/koopman_operator"
+    
+    path1 = os.path.join(linux_dir2, f"Autoencoder_model_params{i}.pth")
+    path2 = os.path.join(windows_dir1, f"Autoencoder_model_params{i}.pth")
+    path3 = os.path.join(colab_dir1, f"Autoencoder_model_params{i}.pth")
+    path4 = os.path.join(colab_dir2, f"Autoencoder_model_params{i}.pth")
+    path5 = os.path.join(windows_dir2, f"Autoencoder_model_params{i}.pth")
+    path6 = os.path.join(ucloud, f"Autoencoder_model_params{i}.pth")
+
     if os.path.exists(path1):
-        return path1
+        chosen_path = path1
     elif os.path.exists(path2):
-        return path2
+        chosen_path = path2
     elif os.path.exists(path3):
-        return path3
+        chosen_path = path3
+    elif os.path.exists(path4):
+        chosen_path = path4  
     elif os.path.exists(path5):
-        return path5
+        chosen_path = path5     
     else:
-        return path4
+        chosen_path = path6
+    print("Using model path:", chosen_path)
+    return chosen_path
 
 def set_requires_grad(layers, requires_grad):
     for param in layers:
