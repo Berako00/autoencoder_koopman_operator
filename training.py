65cbceb2cdbfdc214f516c2a559e71fbd5fd4e24
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-04-25 10:09:18 +0200

    Update training.py

--- training.py
+++ training.py
@@ -10,7 +10,7 @@ from help_func import self_feeding, enc_self_feeding, set_requires_grad, get_mod
 from loss_func import total_loss, total_loss_forced, total_loss_unforced
 from nn_structure import AUTOENCODER
 
-def trainingfcn(eps, check_epoch, lr, batch_size, S_p, T, alpha, Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons, Num_u_Obsv, Num_u_Neurons, Num_hidden_x_encoder, Num_hidden_x_decoder, Num_hidden_u_encoder, Num_hidden_u_decoder, train_tensor, test_tensor, M, device=None):
+def trainingfcn(eps, check_epoch, lr, batch_size, S_p, T, dt, alpha, Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons, Num_u_Obsv, Num_u_Neurons, Num_hidden_x_encoder, Num_hidden_x_decoder, Num_hidden_u_encoder, Num_hidden_u_decoder, train_tensor, test_tensor, M, device=None):
 
   if device is None:
       device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
